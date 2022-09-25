09d88ba40d0c91f602adcbfbfd4c87e06fcfdea3
ViniciusAPacheco <103275060+ViniciusAPacheco@users.noreply.github.com>
2022-09-25 20:19:17 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int dia;
+    int mes;
+    int ano;
+    
+    printf ("Digite a data de nascimento:\n");
+    scanf ("%i", &dia);
+    printf ("Digite o mes de nascimento:\n");
+    scanf ("%i", &mes);
+    printf ("Digite o ano de nascimento:\n");
+    scanf ("%i", &ano);
+        diaEmes + ano = x
+        //dois primeiros digitos do ano mais os dois ultimos, dividios por 5//
+    for x=0 {
+    printf ("timido")
+    }
+    for x=1 {
+    printf ("sonhador")
+    }
+    for x=2 {
+    printf ("paquerador")
+    }
+    for x=3 {
+    printf ("atraente")
+    }
+    for x=4 {
+    printf ("irresistivel")
+    }
+    printf ("O perfil é\n", &x);
+
+  return(0);
+}
